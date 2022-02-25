5d5e3e323bb36a46066d0f182e8b5723fa188836
saharshg29 <saharshg29@gmail.com>
2022-02-25 16:17:34 +0530

    Connection established with mongo DB

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('working')
+const express = require('express')
+const mongoose = require('mongoose')
+const URI = require('./KEY')
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+const DB = URI.URI
+
+mongoose.connect(DB)
+    .then(() => {
+        console.log('Conneection with mongo Db established')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.get('/', (rer, res) => {
+    res.send('Server is working')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is working on ${PORT}`)
+})
