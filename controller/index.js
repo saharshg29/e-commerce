92671252093c78b671e72cf4fe8c28722afdecc9
saharshg29 <saharshg29@gmail.com>
2022-02-26 15:58:20 +0530

    started working with view

--- controller/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const buyer = require('./Routes/buyer')
-const app = express.Router()
-
-app.use('/buyer', buyer)
-
-module.exports = app
--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="sticky-top border mb-2">Top Bar</div>
+      <div className="row border">
+        <div className="col-md  border  ">Left bar </div>
+        <div className="col-md-8 border">Main bar </div>
+        <div className="col-md  border ">Right bar </div>
+      </div>
+      <div className="sticky-bottom border mt-2">Footer </div>
+    </>
   );
 }
 
