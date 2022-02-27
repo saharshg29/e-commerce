93bcdb42949592cd44afa10b1c860533a64b04ba
saharshg29 <saharshg29@gmail.com>
2022-02-27 11:23:34 +0530

    added screens

---
+++ view/src/Components/Footer.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
+export default function Footer() {
+  return (
+    <MDBFooter
+      className="text-center text-white"
+      style={{ backgroundColor: "#f1f1f1" }}
+    >
+      <MDBContainer className="pt-4">
+        <section className="mb-4">
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fab fa-facebook-f" />
+          </a>
+
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fa-twitter" />
+          </a>
+
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fa-google" />
+          </a>
+
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fa-instagram" />
+          </a>
+
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fa-linkedin" />
+          </a>
+
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <MDBIcon fab className="fa-github" />
+          </a>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center text-dark p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2020 Copyright:
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
