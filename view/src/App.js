d5f90699542bf28210d293d6552168b03f97a1b4
saharshg29 <saharshg29@gmail.com>
2022-02-27 17:05:34 +0530

    tried using buyer screen

--- view/src/App.js
+++ view/src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Components/Footer";
 import General from "./Screens/General/General";
+import Buyer from "./Screens/Buyer/App";
 
 function App() {
   return (
     <>
-      <div>
+      {/* <div>
         
         <General />
-        <Footer />
-      </div>
+       
+      </div> */}
+
+
+      <Buyer />
+      <Footer />
     </>
   );
 }
