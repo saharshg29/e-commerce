ee6f0598d8e98500d4af140bdbdca11a663d0e01
saharshg29 <saharshg29@gmail.com>
2022-02-27 12:24:21 +0530

    waiting for landing page from soumya

---
+++ view/src/Screens/General/General.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Normal from '../../Components/Navbar/Normal'
+
+export default function General() {
+  return (
+    <>
+        <Normal></Normal>
+    </>
+  )
+}
