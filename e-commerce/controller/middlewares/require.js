97ea522e76410e5e9850f89a5c8507b7002ed45f
saharshg29 <vtu19110@veltech.edu.in>
2022-03-01 20:12:28 +0530

    jwt checker

---
+++ e-commerce/controller/middlewares/require.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const { JWT_TOKEN } = require('../config/keys')
+const mongoose = require('mongoose')
+const Customer = mongoose.model("Customer")
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers
+    if (!authorization) {
+        return res.status(401).json({ error: "You must be loged in" })
+    }
+    const token = authorization
+    jwt.verify(token, JWT_TOKEN, (err, payload) => {
+        if (err) {
+            return res.status(401).json({ error: "You must be authorized" })
+        }
+
+        const { _id } = payload
+        User.findById(_id)
+            .then(userData => {
+                req.user = userData
+                next()
+            })
+    })
+}
