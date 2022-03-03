78cbb046b14209b8a8832e989137d3896c96236f
saharshg29 <vtu19110@veltech.edu.in>
2022-03-03 22:21:24 +0530

    small warnings removed

--- view/src/Components/Navbar/Normal.jsx
+++ view/src/Components/Navbar/Normal.jsx
@@ -5,20 +5,20 @@ export default function Normal() {
   let navigate = useNavigate();
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <span class="navbar-brand me-2" onClick={() => navigate('/')}>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <span className="navbar-brand me-2" onClick={() => navigate("/")}>
             <img
               src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
               height="16"
               alt="MDB Logo"
               loading="lazy"
-              style={{ marginTop: " -1px;" }}
+              style={{ marginTop: " -1px" }}
             />
           </span>
 
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-mdb-toggle="collapse"
             data-mdb-target="#navbarButtonsExample"
@@ -26,33 +26,33 @@ export default function Normal() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i class="fas fa-bars"></i>
+            <i className="fas fa-bars"></i>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarButtonsExample">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <div className="collapse navbar-collapse" id="navbarButtonsExample">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
 
-            <div class="d-flex align-items-center">
+            <div className="d-flex align-items-center">
               <button
                 type="button"
-                class="btn btn-link px-3 me-2"
+                className="btn btn-link px-3 me-2"
                 onClick={() => navigate("/signin")}
               >
                 Login
               </button>
               <button
                 type="button"
-                class="btn btn-primary me-3"
+                className="btn btn-primary me-3"
                 onClick={() => navigate("/add-buyer")}
               >
                 Sign Up
               </button>
               <a
-                class="btn btn-dark px-3"
+                className="btn btn-dark px-3"
                 href="https://github.com/mdbootstrap/mdb-ui-kit"
                 role="button"
               >
-                <i class="fab fa-github"></i>
+                <i className="fab fa-github"></i>
               </a>
             </div>
           </div>
